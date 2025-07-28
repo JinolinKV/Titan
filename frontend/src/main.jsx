1387f380139db274dc4ebf36cc66383d6fa1995b
Jinolinkv <jinolinkv@gmail.com>
2025-07-28 10:49:21 +0530

    Fix Vite base path for Render deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <WishlistProvider>  
       <CartProvider>
-        <BrowserRouter basename="/Titan">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </CartProvider>
